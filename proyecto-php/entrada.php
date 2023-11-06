027206c0384121927f420e17d3431baf4db14244
danie <danie@DESKTOP-8BC9KNM>
2023-11-06 08:57:45 +0100

    Edición de entradas

--- proyecto-php/entrada.php
+++ proyecto-php/entrada.php
@@ -24,7 +24,7 @@
     </p>
     
     <?php if(isset($_SESSION["usuario"]) && $_SESSION['usuario']['id'] == $entrada_actual['usuario_id']): ?>
-        <a href="editar-entrada.php" class="boton boton-verde">Editar entrada</a>
+        <a href="editar-entrada.php?id=<?=$entrada_actual['id']?>" class="boton boton-verde">Editar entrada</a>
         <a href="borrar-entrada.php?id=<?=$entrada_actual['id']?>" class="boton">Borrar entrada</a>
     <?php endif; ?>
 </div><!--Fin principal-->
