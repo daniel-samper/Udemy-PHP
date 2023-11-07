9978fcee7259a7fcf139618e9c2fa68fd6ad6c90
Daniel <Daniel@Daniel>
2023-11-07 14:23:56 +0100

    PHP-POO 02-Constructor

--- php-poo/02-constructor/index.php
+++ php-poo/02-constructor/index.php
@@ -3,5 +3,11 @@
 require_once 'coche.php';
 
 $coche = new Coche("Amarillo","Renault","Clio",150,200,5);
+$coche2 = new Coche("Verde","Seat","Panda",250,200,5);
+$coche3 = new Coche("Azul","Citroen","Xara",100,220,4);
+$coche4 = new Coche("Rojo","Mercedes","Clase A",350,100,3);
 
-var_dump($coche);
+$coche->color="ROSA";
+$coche->setMarca("Audi");
+
+echo $coche->mostrarInformacion($coche2);
