f8767ada62f6bec62f4eab5d2d61cf32825c5c19
danie <danie@DESKTOP-8BC9KNM>
2023-11-08 08:27:51 +0100

    PHP POO Namespaces

--- php-poo/12-autoload/index.php
+++ php-poo/12-autoload/index.php
@@ -7,6 +7,7 @@ require_once 'categoria.php';
 require_once 'entrada.php';
  */
 
+/*
 $usuario = new Usuario();
 echo $usuario->nombre;
 echo"<br>";
@@ -14,3 +15,30 @@ echo $usuario->email;
 
 $categoria = new Categoria();
 echo "<br>".$categoria->nombre;
+ */
+
+// ESPACIOS DE NOMBRES Y PAQUETES
+
+use MisClases\Usuario;
+use MisClases\Categoria;
+use MisClases\Entrada;
+use PanelAdministrador\Usuario as UsuarioAdmin;
+
+class Principal{
+    public $udustio;
+    public $categoria;
+    public $entrada;
+    
+    public function __construct() {
+        $this->usuario = new Usuario();
+        $this->categoria = new Categoria();
+        $this->entrada = new Entrada();
+    }
+}
+// Objeto Principal
+$principal = new Principal();
+var_dump($principal->usuario);
+
+// Otro paquete
+$usuario = new UsuarioAdmin;
+var_dump($usuario);
