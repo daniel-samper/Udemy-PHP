e84879e10a5d18f86311b4318f073da88dc9b60c
danie <danie@DESKTOP-8BC9KNM>
2023-11-09 19:34:19 +0100

    PHP Librerias - Thumnadils

--- librerias-php/manipular-imagenes/index.php
+++ librerias-php/manipular-imagenes/index.php
@@ -5,7 +5,15 @@ $foto_original = 'mifoto.jpg';
 $guardr_en = 'fotomodificada.jpg';
 
 $thumb = new PHPThumb\GD($foto_original);
-$thumb->resize(50,50);
+
+// Redimensionar
+$thumb->resize(250,250);
+
+// Recortar
+$thumb->crop(50, 50, 120, 120);
+
+
+
 $thumb->show();
 $thumb->save($guardr_en);
 
