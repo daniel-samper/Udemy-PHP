6f76a1ebd9ec25be7a53cc5713810304620e0b5e
danie <danie@DESKTOP-8BC9KNM>
2023-11-09 19:46:45 +0100

    PHP Librerias - Console.log con FirePHP

---
+++ librerias-php/depurar/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+$frutas = array("manzanas" , "naranjas", "sandias");
+
+\FB::log($frutas);
+
+$nombres = array("ejecutivo" => "Antonio","empleado" => "juan");
+
+\FB::log($nombres);
+
+echo "Hola Mundo !!".$nombres['ejecutivo'];
+
+\FB::log("Muéstrame en consola");
+
