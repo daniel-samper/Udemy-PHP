fb1bb90799607ad24250e9a62c586f1c48c7bd35
Daniel <Daniel@Daniel>
2023-11-10 17:51:01 +0100

    Proyecto PHP POO - Maquetación barra lateral

--- proyecto-php-poo/index.php
+++ proyecto-php-poo/index.php
@@ -46,6 +46,7 @@
                 <!-- BARRA LATERAL -->
                 <aside id="lateral">
                     <div id="login" class="block_aside">
+                        <h3>Entrar a la web</h3>
                         <form action="#" method="post">
                             <label for="email">Email</label>
                             <input type="email" name="email">
