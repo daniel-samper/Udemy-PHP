b999d252744ebbda5b6478a96025a4e1eacf29c5
danie <danie@DESKTOP-8BC9KNM>
2023-11-12 21:55:48 +0100

    Proyecto PHP POO - Formulario de registro de usuarios

--- proyecto-php-poo/controllers/UsuarioController.php
+++ proyecto-php-poo/controllers/UsuarioController.php
@@ -1,7 +1,16 @@
 <?php
 
 class usuarioController{
+    
     public function index() {
+        echo "controlador Usuarios, Acción index";
+    }
+    
+    public function registro() {
+        require_once 'views/usuario/registro.php';
+    }
+    
+    public function save() {
         echo "controlador Usuarios, Acción index";
     }
 }
---
+++ proyecto-php-poo/views/usuario/registro.php
@@ -0,0 +1,19 @@
+<h1>Registro de usuarios</h1>
+
+<form action="index-php?controller=usuario&action=save" method="POST">
+    
+    <label for="nombre">Nombre</label>
+    <input type="text" name="nombre" required />
+    
+    <label for="apellidos">Apellidos</label>
+    <input type="text" name="apellidos" required />
+    
+    <label for="email">Email</label>
+    <input type="email" name="email" required />
+    
+    <label for="password">Contraseña</label>
+    <input type="text" name="password" required />
+    
+    <input type="submit" value="Registrarse" />
+    
+</form>
