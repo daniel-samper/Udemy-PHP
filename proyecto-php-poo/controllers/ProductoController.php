a5c12495301a3fb2c15df03db24e50330122b437
Daniel <Daniel@Daniel>
2023-11-16 17:01:31 +0100

    Proyecto PHP POO - Subir imágenes

--- proyecto-php-poo/controllers/ProductoController.php
+++ proyecto-php-poo/controllers/ProductoController.php
@@ -41,13 +41,28 @@ class productoController{
                 $producto->setStock($stock);
                 $producto->setCategoria_id ($categoria);
                 
+                // Guardar la imagen
+                $file = $_FILES['imagen'];
+                $filename = $file['name'];
+                $mimetype = $file['type'];
+                
+                if($mimetype == "image/jpg" || $mimetype == "image/jpeg" || $mimetype == "image/png" || $mimetype == "image/gif"):
+                    
+                    if(!is_dir('uploads/images')):
+                        mkdir ('uploads/images', 0777, true);
+                    endif;
+                    
+                move_uploaded_file($file['tmp_name'], 'uploads/images/'.$filename);
+                $producto->setImagen($filename);
+                    
+                endif;
                 $save = $producto->save ();
                 
                 if($save):
                     $_SESSION['producto'] = "complete";          
                 else:      
                     $_SESSION['producto'] = "failed";
-                endif;;
+                endif;
             else:
                 $_SESSION['producto'] = "failed";
             endif;
