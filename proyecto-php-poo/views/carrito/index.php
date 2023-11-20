5bed547fa1d559b8a93ef848d656436d624007d0
Daniel <Daniel@Daniel>
2023-11-20 13:05:05 +0100

    Proyecto PHP POO - Estadísticas del carrito

--- proyecto-php-poo/views/carrito/index.php
+++ proyecto-php-poo/views/carrito/index.php
@@ -32,3 +32,10 @@
         </tr>
     <?php endforeach; ?>
 </table>
+<br>
+<div class="total-carrito">
+    <?php $stats = Utils::statsCarrito(); ?>
+    <h3>Precio total; <?=$stats['total']?> $</h3>
+
+    <a href="" class="button button-pedido">Hacer pedido</a>
+</div>
