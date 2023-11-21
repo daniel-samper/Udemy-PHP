1ca86ca1f5b848934c64de0ed04c894ffc8a6de4
Daniel <Daniel@Daniel>
2023-11-21 16:00:31 +0100

    Proyecto PHP POO - Confirmación de pedido

--- proyecto-php-poo/views/pedido/confirmado.php
+++ proyecto-php-poo/views/pedido/confirmado.php
@@ -1,23 +1,34 @@
-<?php if(isset($_SESSION['pedido']) && $_SESSION['pedido'] == 'complete'):?>
-<h1>Tu pedido se ha confirmado</h1>
-<p>
-    Tu pedido ha sido guardado con éxito, uan vez que realices la transferencia
-    bancaria a la cuenta 7382947289239ADD con el coste del pedido, será procesado y enviado.
-</p>
-<br>
-<h3>
-    Datos del pedido:
-</h3>
-<pre>
-Número de pedido:
-Total a pagar:
-Productos:
-</pre>
+<?php if (isset($_SESSION['pedido']) && $_SESSION['pedido'] == 'complete'): ?>
+    <h1>Tu pedido se ha confirmado</h1>
+    <p>
+        Tu pedido ha sido guardado con éxito, uan vez que realices la transferencia
+        bancaria a la cuenta 7382947289239ADD con el coste del pedido, será procesado y enviado.
+    </p>
+    <br>
+    <?php if (isset($pedido)): ?>
+        <h3>
+            Datos del pedido:
+        </h3>
 
-<?php elseif(isset($_SESSION['pedido']) && $_SESSION['pedido'] != 'complete'): ?>
-<p>
-    Tu pedido NO ha podido procesarse.
-</p>
+        Número de pedido: <?=$pedido->id ?> <br>
+        Total a pagar: <?=$pedido->coste ?> $<br>
+        Productos:
+        
+        <?php while($producto = $productos->fetch_object()): ?>
+        <ul>
+            <li>
+                <?=$producto->nombre?> - <?=$producto->precio?> $ - x<?=$producto->unidades?>
+            </li>
+        </ul>
+        
+        <?php endwhile; ?>
+
+    <?php endif; ?>
+
+<?php elseif (isset($_SESSION['pedido']) && $_SESSION['pedido'] != 'complete'): ?>
+    <p>
+        Tu pedido NO ha podido procesarse.
+    </p>
 <?php endif; ?>
 
 
