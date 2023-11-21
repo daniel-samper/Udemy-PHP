f0f46f62a0b72a8a8461c74dc5e0ecc6dda617c1
Daniel <Daniel@Daniel>
2023-11-21 16:12:48 +0100

    Proyecto PHP POO - Confirmación de pedido

--- proyecto-php-poo/views/pedido/confirmado.php
+++ proyecto-php-poo/views/pedido/confirmado.php
@@ -10,19 +10,39 @@
             Datos del pedido:
         </h3>
 
-        Número de pedido: <?=$pedido->id ?> <br>
-        Total a pagar: <?=$pedido->coste ?> $<br>
+        Número de pedido: <?= $pedido->id ?> <br>
+        Total a pagar: <?= $pedido->coste ?> $<br>
         Productos:
-        
-        <?php while($producto = $productos->fetch_object()): ?>
-        <ul>
-            <li>
-                <?=$producto->nombre?> - <?=$producto->precio?> $ - x<?=$producto->unidades?>
-            </li>
-        </ul>
-        
-        <?php endwhile; ?>
 
+        <table>
+            <tr>
+                <th>Imagen</th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Unidades</th>
+            </tr>
+            <?php while ($producto = $productos->fetch_object()): ?>
+                <tr>
+                    <td>
+                        <?php if ($producto->imagen != null): ?>  
+                            <img src="<?= base_url ?>uploads/images/<?= $producto->imagen ?>"  class="img_carrito"/>
+                        <?php else: ?>
+                            <img src="<?= base_url ?>assets/img/camiseta.png" class="img_carrito"/>
+                        <?php endif; ?>
+                    </td>
+                    <td>
+                        <a href="<?= base_url ?>producto/ver&id=<?= $producto->id ?>"><?= $producto->nombre ?></a>
+                    </td>
+                    <td>
+                        <?= $producto->precio ?>
+                    </td>
+                    <td>
+                        <?= $producto->unidades ?>
+                    </td>
+                </tr>
+
+            <?php endwhile; ?>
+        </table>
     <?php endif; ?>
 
 <?php elseif (isset($_SESSION['pedido']) && $_SESSION['pedido'] != 'complete'): ?>
