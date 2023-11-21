b17e1e6f1be510d6c7d3e11db74fd73205b310c2
Daniel <Daniel@Daniel>
2023-11-21 14:28:23 +0100

    Proyecto PHP POO - Confirmación de pedido

--- proyecto-php-poo/views/pedido/confirmado.php
+++ proyecto-php-poo/views/pedido/confirmado.php
@@ -2,8 +2,18 @@
 <h1>Tu pedido se ha confirmado</h1>
 <p>
     Tu pedido ha sido guardado con éxito, uan vez que realices la transferencia
-    bancaria con e lcoste del pedido, será procesado y enviado.
+    bancaria a la cuenta 7382947289239ADD con el coste del pedido, será procesado y enviado.
 </p>
+<br>
+<h3>
+    Datos del pedido:
+</h3>
+<pre>
+Número de pedido:
+Total a pagar:
+Productos:
+</pre>
+
 <?php elseif(isset($_SESSION['pedido']) && $_SESSION['pedido'] != 'complete'): ?>
 <p>
     Tu pedido NO ha podido procesarse.
