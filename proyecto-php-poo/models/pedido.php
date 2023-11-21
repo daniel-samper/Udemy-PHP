66187f3e6e79190aebdcadd5b21120755bb0666f
Daniel <Daniel@Daniel>
2023-11-21 17:26:24 +0100

    Proyecto PHP POO - Mis pedidos

--- proyecto-php-poo/models/pedido.php
+++ proyecto-php-poo/models/pedido.php
@@ -108,6 +108,13 @@ class Pedido{
         $pedido = $this->db->query($sql);
         return $pedido->fetch_object();
     }
+           
+    public function getAllByUser() {
+        $sql = "SELECT p.* FROM pedidos p "
+                . "WHERE p.usuario_id = {$this->getUsuario_id()} ORDER BY id DESC";
+        $pedido = $this->db->query($sql);
+        return $pedido;
+    }
     
     public function getProductosByPedido($id){
 //        $sql = "SELECT * FROM productos WHERE id IN "
